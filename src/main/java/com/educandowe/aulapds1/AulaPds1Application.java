ec557afebf6c053bf73858c0c63b15165303f3d0
Camila Mariano <milacvm@hotmail.com>
2019-08-20 22:29:46 -0300

    Projeto Criado

---
+++ src/main/java/com/educandowe/aulapds1/AulaPds1Application.java
@@ -0,0 +1,13 @@
+package com.educandowe.aulapds1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaPds1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaPds1Application.class, args);
+	}
+
+}
