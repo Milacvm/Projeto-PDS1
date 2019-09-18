bc40334d88945ada3119cc0a609fb4184beba50f
Camila Mariano <milacvm@hotmail.com>
2019-09-17 22:15:39 -0300

    Category

--- src/main/java/com/educandowe/aulapds1/services/CategoryService.java
+++ src/main/java/com/educandowe/aulapds1/services/CategoryService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandowe.aulapds1.entities.Category;
 import com.educandowe.aulapds1.repositories.CategoryRepository;
+import com.educandowe.aulapds1.services.exceptions.DatabaseException;
+import com.educandowe.aulapds1.services.exceptions.ResourceNotFoundException;
 
 @Service 
 public class CategoryService {
@@ -22,4 +26,16 @@ public class CategoryService {
     	Optional<Category> obj =  repository.findById(Id);
     	return obj.get();
     }
+    
+    public Category insert (Category obj) {
+    	return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+    	repository.deleteById(id);
+    } } catch (EmptyResultDataAccessException e) {
+    	throw new ResourceNotFoundException(id);
+    } catch (DataIntegrityViolationException e) {
+    	throw new DatabaseException(e.getMessage());
 }
+
