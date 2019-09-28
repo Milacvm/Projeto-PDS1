1a58c0c943d4a363fa0d01a0d8acbb1eba08b819
Camila Mariano <milacvm@hotmail.com>
2019-09-27 21:30:22 -0300

    Desafio 2

--- src/main/java/com/educandowe/aulapds1/services/ProductService.java
+++ src/main/java/com/educandowe/aulapds1/services/ProductService.java
@@ -2,10 +2,12 @@ package com.educandowe.aulapds1.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.educandowe.aulapds1.dto.ProductDTO;
 import com.educandowe.aulapds1.entities.Product;
 import com.educandowe.aulapds1.repositories.ProductRepository;
 
@@ -15,11 +17,13 @@ public class ProductService {
     @Autowired
 	private ProductRepository repository;
     
-    public List <Product> findAll() {
-       return repository.findAll();	
+    public List <ProductDTO> findAll() {
+      List <Product> list = repository.findAll();	
+      return list.stream().map(e -> new ProductDTO(e)).collect(Collectors.toList());
     }
-    public Product findById(Long Id) {
+    public ProductDTO findById(Long Id) {
     	Optional<Product> obj =  repository.findById(Id);
-    	return obj.get();
+    	Product entity = obj.get();
+    	return new ProductDTO(entity);
     }
 }
