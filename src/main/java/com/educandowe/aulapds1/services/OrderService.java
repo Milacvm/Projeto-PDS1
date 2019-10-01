fbf547f24781b6234ca4a4cfd349fdb47802f962
Camila Mariano <milacvm@hotmail.com>
2019-10-01 20:37:33 -0300

    OrderDTO

--- src/main/java/com/educandowe/aulapds1/services/OrderService.java
+++ src/main/java/com/educandowe/aulapds1/services/OrderService.java
@@ -2,12 +2,15 @@ package com.educandowe.aulapds1.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.educandowe.aulapds1.dto.OrderDTO;
 import com.educandowe.aulapds1.entities.Order;
 import com.educandowe.aulapds1.repositories.OrderRepository;
+import com.educandowe.aulapds1.services.exceptions.ResourceNotFoundException;
 
 @Service 
 public class OrderService {
@@ -15,11 +18,14 @@ public class OrderService {
     @Autowired
 	private OrderRepository repository;
     
-    public List <Order> findAll() {
-       return repository.findAll();	
-    }
-    public Order findById(Long Id) {
-    	Optional<Order> obj =  repository.findById(Id);
-    	return obj.get();
-    }
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findAll();
+		return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
+	}
+
+	public OrderDTO findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		Order entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
+		return new OrderDTO(entity);
+	}
 }
