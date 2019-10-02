94174b8a92ec59dc5d96a71375285d98fcb885bd
Camila Mariano <milacvm@hotmail.com>
2019-10-01 21:27:51 -0300

    Product Insert

--- src/main/java/com/educandowe/aulapds1/services/ProductService.java
+++ src/main/java/com/educandowe/aulapds1/services/ProductService.java
@@ -6,9 +6,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.educandowe.aulapds1.dto.CategoryDTO;
+import com.educandowe.aulapds1.dto.ProductCategoriesDTO;
 import com.educandowe.aulapds1.dto.ProductDTO;
+import com.educandowe.aulapds1.entities.Category;
 import com.educandowe.aulapds1.entities.Product;
+import com.educandowe.aulapds1.repositories.CategoryRepository;
 import com.educandowe.aulapds1.repositories.ProductRepository;
 
 @Service 
@@ -16,6 +21,9 @@ public class ProductService {
 	
     @Autowired
 	private ProductRepository repository;
+    
+    @Autowired
+    private CategoryRepository categoryRepository;
     
     public List <ProductDTO> findAll() {
       List <Product> list = repository.findAll();	
@@ -26,4 +34,21 @@ public class ProductService {
     	Product entity = obj.get();
     	return new ProductDTO(entity);
     }
+    
+    @Transactional
+	public ProductDTO insert(ProductCategoriesDTO dto) {
+		Product entity = dto.toEntity();
+		setProductCategories(entity, dto.getCategories());
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+		
+	}
+	
+	private void setProductCategories(Product entity, List<CategoryDTO> categories) {
+	    entity.getCategories().clear();
+		for (CategoryDTO dto : categories) {
+			Category category = categoryRepository.getOne(dto.getId());
+			entity.getCategories().add(category);
+	 }
+  }
 }
