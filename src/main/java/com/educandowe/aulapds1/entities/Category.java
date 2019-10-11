bec4a76349ed2fc474fe822edf05633d6aff579f
Camila Mariano <milacvm@hotmail.com>
2019-10-11 20:10:12 -0300

    Auditing data: PreUpdate & PrePersist

--- src/main/java/com/educandowe/aulapds1/entities/Category.java
+++ src/main/java/com/educandowe/aulapds1/entities/Category.java
@@ -1,6 +1,7 @@
 package com.educandowe.aulapds1.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,6 +10,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -17,18 +20,21 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Table(name = "tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
 	@JsonIgnore
 	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
-	
+
+	private Instant createdAt;
+	private Instant updatedAt;
+
 	public Category() {
-}
+	}
 
 	public Category(Long id, String name) {
 		super();
@@ -51,11 +57,31 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public Set<Product> getProducts() {
 		return products;
 	}
-	
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		Instant now = Instant.now();
+		updatedAt = now;
+		createdAt = now;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
