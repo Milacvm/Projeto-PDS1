1bf14fed782d8c0faceb3b7311b296f36ee08ef4
Camila Mariano <milacvm@hotmail.com>
2019-10-15 22:19:33 -0300

    Data model

---
+++ src/main/java/com/educandowe/aulapds1/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.educandowe.aulapds1.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandowe.aulapds1.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
