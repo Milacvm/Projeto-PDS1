6d87f43ab9e8eb445bdfeb1b1893112c2d9f739e
Camila Mariano <milacvm@hotmail.com>
2019-10-22 21:20:16 -0300

    Authenticaton

--- src/main/java/com/educandowe/aulapds1/security/JWTUtil.java
+++ src/main/java/com/educandowe/aulapds1/security/JWTUtil.java
@@ -3,10 +3,13 @@ package com.educandowe.aulapds1.security;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
+
+@Component
 public class JWTUtil {
 	
 	@Value("${jwt.secret}")
--- src/main/java/com/educandowe/aulapds1/services/AuthService.java
+++ src/main/java/com/educandowe/aulapds1/services/AuthService.java
@@ -1,5 +1,34 @@
 package com.educandowe.aulapds1.services;
 
-public class AuthService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
 
+import com.educandowe.aulapds1.dto.CredentialsDTO;
+import com.educandowe.aulapds1.dto.TokenDTO;
+import com.educandowe.aulapds1.security.JWTUtil;
+import com.educandowe.aulapds1.services.exceptions.JWTAuthenticationException;
+
+
+@Service
+public class AuthService {
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	public TokenDTO authenticate(CredentialsDTO dto) {
+		try {
+		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken (dto.getEmail(), dto.getPassword());
+	    authenticationManager.authenticate(authToken);
+	    String token = jwtUtil.generateToken(dto.getEmail());
+	    return new TokenDTO(dto.getEmail(),token);
+	}catch (AuthenticationException e) {
+	    throw new JWTAuthenticationException("Bad credentials");	
+	}
+  }
 }
